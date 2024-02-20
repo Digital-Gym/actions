f5e264b8c902f947a4fd55611db8050d0f2fe086
muminovbob <bobomurodm2004@gmail.com>
2024-02-20 18:28:42 +0500

    [BREAKING CHANGE] Introduction of PrimeVue

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,4 +1,7 @@
-import './assets/base.css'
+// import './assets/base.css'
+import 'primevue/resources/themes/aura-light-blue/theme.css'
+import 'primeicons/primeicons.css'
+import 'primeflex/primeflex.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
@@ -6,8 +9,11 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+import PrimeVue from 'primevue/config';
+
 const app = createApp(App)
 
+app.use(PrimeVue, { /* options */ });
 app.use(createPinia())
 app.use(router)
 
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,18 +1,83 @@
 <script setup>
+import {ref} from 'vue'
 
+import Button from 'primevue/button';
+import Menu from 'primevue/menu';
+
+import DataTable from 'primevue/datatable';
+import Column from 'primevue/column';
+
+const menu = ref();
+
+const items = ref([
+  {
+    label: "Main",
+    items: [
+      {label: 'Home', icon: 'pi pi-plus' },
+      {label: 'Search', icon: 'pi pi-search' },
+      {label: 'Drop database', icon: 'pi pi-database'}
+    ]
+  },
+  {
+    label: "Misc",
+    items: [
+      {label: 'Donate', icon: "pi pi-gift"}
+    ]
+  }
+]);
+
+const toggle = (event) => {
+    menu.value.toggle(event);
+};
+
+const formatCurrency = (value) => {
+    return value.toLocaleString('en-US', { style: 'currency', currency: 'USD' });
+};
+
+const data = ref([
+  {name: "Bob", age: 20, networth: 1000000, isMarried: "No"},
+  {name: "Xonda", age: 20, networth: 696969, isMarried: "No bitches"},
+  {name: "Tema", age: 20, networth: 228228, isMarried: "No hoes"}
+])
 </script>
 
 <template>
-  <main>
+  <div class="sidebar flex justify-content-center">
+      <Button type="button" icon="pi pi-ellipsis-v" @click="toggle" aria-haspopup="true" aria-controls="overlay_menu" />
+      <Menu ref="menu" id="overlay_menu" :model="items" :popup="true">
+        <template #submenuheader="{ item }">
+          <span class="text-secondary font-bold text-sm">{{ item.label }}</span>
+        </template>
+      </Menu>
+  </div>
+
+  <main class="flex flex-column justify-content-center align-items-center">
+    
     <h1>Home</h1>
-    <RouterLink class='btn' :to="{name: 'about'}">About</RouterLink>
+    <RouterLink class='btn' :to="{name: 'about'}">
+      <Button label="About" rounded />
+    </RouterLink>
+
+    <DataTable :value="data" tableStyle="min-width: 50rem">
+      <Column field="name" header="Name"></Column>
+      <Column field="age" header="Age"></Column>
+      <Column field="networth" header="Wealth">
+        <template #body="slotProps">
+            {{ formatCurrency(slotProps.data.networth) }}
+        </template>
+      </Column>
+      <Column field="isMarried" header="Is Married?"></Column>
+    </DataTable>
+
   </main>
+  
 </template>
 
 <style scoped>
-.btn{
-  text-decoration: none;
-  color: white;
+.sidebar{
+  position: fixed;
+  left: 3%;
+  top: 6%;
 }
 
 </style>
